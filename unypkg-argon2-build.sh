ba475289a5e4a6812b09de397b4b7005d333e7ea
michacassola <mich@casso.la>
2024-06-02 21:55:52 +0200

    Update unypkg-argon2-build.sh

--- unypkg-argon2-build.sh
+++ unypkg-argon2-build.sh
@@ -35,13 +35,13 @@ mkdir -pv /uny/sources
 cd /uny/sources || exit
 
 pkgname="argon2"
-pkggit="https://github.com/argon2/argon2.git refs/tags/*"
+pkggit="https://github.com/P-H-C/phc-winner-argon2.git refs/tags/*"
 gitdepth="--depth=1"
 
 ### Get version info from git remote
 # shellcheck disable=SC2086
-latest_head="$(git ls-remote --refs --tags --sort="v:refname" $pkggit | grep -E "v[0-9.]+$" | tail --lines=1)"
-latest_ver="$(echo "$latest_head" | grep -o "v[0-9.].*" | sed "s|v||")"
+latest_head="$(git ls-remote --refs --tags --sort="v:refname" $pkggit | grep -E "/[0-9.]+$" | tail --lines=1)"
+latest_ver="$(echo "$latest_head" | grep -o "/[0-9.].*" | sed "s|/||")"
 latest_commit_id="$(echo "$latest_head" | cut --fields=1)"
 
 version_details
@@ -77,12 +77,12 @@ get_include_paths
 
 unset LD_RUN_PATH
 
-./configure \
-    --prefix=/uny/pkg/"$pkgname"/"$pkgver"
+#./configure \
+#    --prefix=/uny/pkg/"$pkgname"/"$pkgver"
 
-make -j"$(nproc)"
-make -j"$(nproc)" check 
-make -j"$(nproc)" install
+make -j"$(nproc)" PREFIX=/uny/pkg/"$pkgname"/"$pkgver"
+
+make -j"$(nproc)" install PREFIX=/uny/pkg/"$pkgname"/"$pkgver"
 
 ####################################################
 ### End of individual build script
